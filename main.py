4b5aaa0e994963901cf7cbb9103d803cfd854bf8
AntipovVlad <vsantipov@edu.hse.ru>
2021-10-13 21:23:41 +0300

    Map logic added

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import curses
 from os import SEEK_DATA
 import time
-from map_generator import map
-from map_objects import hero, enemy, bomb
+from map_generator import map, map_elements
+from game_objects import hero, enemy, bomb
 from curses import textpad
 
 menu = ['Play', 'Scoreboard', 'Exit']
@@ -25,9 +25,9 @@ def print_menu(stdscr, selected_row_idx):
     stdscr.refresh()
 
 
-def del_foot(stdscr, y, x, t) -> None:
+def add_str(stdscr, y, x, s, t) -> None:
     stdscr.attron(curses.color_pair(t))
-    stdscr.addstr(y, x, ' ')
+    stdscr.addstr(y, x, s)
     stdscr.attroff(curses.color_pair(t))
 
 
@@ -37,29 +37,27 @@ def play(stdscr):
     levels = 1
 
     for _ in range(levels):
-        field, islands = map.generate_map(5)
+        field, rooms, bridges = map.generate_map(5)
         h, w = stdscr.getmaxyx()
         y, x = int(h * 0.1), 0
 
-        spawn = islands[0]
-        spawn.make_visible()
-        lt_y, lt_x, rb_y, rb_x = spawn.get_coordinates()
+        cur_room = rooms[0]
+        cur_room.open()
+        cur_room.activate()
+
+        lt_y, lt_x, rb_y, rb_x = cur_room.get_coordinates()
         hy, hx = (lt_y + rb_y) // 2, (lt_x + rb_x) // 2
 
         for i in range(len(field)):
             for j in range(len(field[0])):
-                if field[i][j].get_type() == 'sea':
-                    stdscr.attron(curses.color_pair(2))
-                    stdscr.addstr(y + i, x + j, ' ')
-                    stdscr.attroff(curses.color_pair(2))
-                elif field[i][j].get_type() == 'ground':
-                    stdscr.attron(curses.color_pair(3))
-                    stdscr.addstr(y + i, x + j, ' ')
-                    stdscr.attroff(curses.color_pair(3))
+                if field[i][j].get_type() == 'stone':
+                    t = 2
+                elif field[i][j].get_type() == 'room':
+                    t = 4 if field[i][j].is_visible() else 2
                 elif field[i][j].get_type() == 'bridge':
-                    stdscr.attron(curses.color_pair(4))
-                    stdscr.addstr(y + i, x + j, ' ')
-                    stdscr.attroff(curses.color_pair(4))
+                    t = 4 if field[i][j].is_visible() else 2
+                
+                add_str(stdscr, y + i, x + j, field[i][j].get_skin(), t)
 
                 stdscr.refresh()
 
@@ -68,31 +66,58 @@ def play(stdscr):
         while 1:
             for obj in objects:
                 if obj == 'Hero':
-                    t = 3 if field[hy][hx].get_type() == 'ground' else 4
-                    stdscr.attron(curses.color_pair(t))
-                    stdscr.addstr(y + hy, hx, '@')
-                    stdscr.attroff(curses.color_pair(t))
+                    t = 4
+                    add_str(stdscr, y + hy, hx, '@', t)
             
             key = stdscr.getch()
 
-            if key == ord('w') and field[hy - 1][hx].get_type() != 'sea':
-                t = 3 if field[hy][hx].get_type() == 'ground' else 4
-                del_foot(stdscr, y + hy, hx, t)
+            if key == ord('w') and field[hy - 1][hx].get_skin() == ' ':
+                t = 4 if field[hy][hx].get_type() == 'room' else 1
+                add_str(stdscr, y + hy, hx, field[hy][hx].get_skin(), t)
+
                 hy -= 1
-            elif key == ord('s') and field[hy + 1][hx].get_type() != 'sea':
-                t = 3 if field[hy][hx].get_type() == 'ground' else 4
-                del_foot(stdscr, y + hy, hx, t)
+            elif key == ord('s') and field[hy + 1][hx].get_skin() == ' ':
+                t = 4 if field[hy][hx].get_type() == 'room' else 1
+                add_str(stdscr, y + hy, hx, field[hy][hx].get_skin(), t)
+
                 hy += 1
-            elif key == ord('a') and field[hy][hx - 1].get_type() != 'sea':
-                t = 3 if field[hy][hx].get_type() == 'ground' else 4
-                del_foot(stdscr, y + hy, hx, t)
+            elif key == ord('a') and field[hy][hx - 1].get_skin() == ' ':
+                t = 4 if field[hy][hx].get_type() == 'room' else 1
+                add_str(stdscr, y + hy, hx, field[hy][hx].get_skin(), t)
+
                 hx -= 1
-            elif key == ord('d') and field[hy][hx + 1].get_type() != 'sea':
-                t = 3 if field[hy][hx].get_type() == 'ground' else 4
-                del_foot(stdscr, y + hy, hx, t)
+            elif key == ord('d') and field[hy][hx + 1].get_skin() == ' ':
+                t = 4 if field[hy][hx].get_type() == 'room' else 1
+                add_str(stdscr, y + hy, hx, field[hy][hx].get_skin(), t)
+                
                 hx += 1
+            elif key == ord('o'):
+                if field[hy][hx].get_type() == 'room':
+                    cur_room = field[hy][hx].get_zone()
+                
+                for dots, bridge in cur_room.open_bridges():
+                    for dot in dots:
+                        field[dot[0]][dot[1]] = map_elements.BridgeBlock(dot[0], dot[1], bridge)
+                        field[dot[0]][dot[1]].set_activator([field[dot[0] - 1][dot[1]], field[dot[0] + 1][dot[1]], field[dot[0]][dot[1] - 1], field[dot[0]][dot[1] + 1]])
+                            
+                for i in range(len(field)):
+                    for j in range(len(field[0])):
+                        if field[i][j].get_type() == 'bridge':
+                            t = 1 if field[i][j].is_visible() else 2
+                        
+                            add_str(stdscr, y + i, x + j, field[i][j].get_skin(), t)
             elif key == curses.KEY_ENTER or key in [10, 13]:
                 break
+            
+            if field[hy][hx].get_type() == 'bridge':
+                field[hy][hx].activate()
+
+                for i in range(len(field)):
+                    for j in range(len(field[0])):
+                        if field[i][j].get_type() == 'room':
+                            t = 4 if field[i][j].is_visible() else 2
+        
+                            add_str(stdscr, y + i, x + j, field[i][j].get_skin(), t)
             
             stdscr.refresh()
 
@@ -102,8 +127,8 @@ def main(stdscr):
     curses.use_default_colors()
 
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
-    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLUE)
-    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_GREEN)
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     current_row_idx = 0
